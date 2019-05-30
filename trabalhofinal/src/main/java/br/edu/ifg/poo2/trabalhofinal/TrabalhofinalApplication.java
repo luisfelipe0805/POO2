f0d1a6e3d0adc2a45a6f024ac91f4f76215bc329
PC <PC@DESKTOP-VNT8D78>
2019-05-29 21:35:27 -0300

    testando commit

--- trabalhofinal/src/main/java/br/edu/ifg/poo2/trabalhofinal/TrabalhofinalApplication.java
+++ trabalhofinal/src/main/java/br/edu/ifg/poo2/trabalhofinal/TrabalhofinalApplication.java
@@ -8,6 +8,9 @@ public class TrabalhofinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrabalhofinalApplication.class, args);
+		
+		
+		System.out.println("teste");
 	}
 
 }
