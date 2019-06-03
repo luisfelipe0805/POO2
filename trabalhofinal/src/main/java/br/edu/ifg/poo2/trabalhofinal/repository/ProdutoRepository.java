4f2d2627445213de6d29a858e413889a4cd2c8b7
PC <PC@DESKTOP-VNT8D78>
2019-06-02 23:21:58 -0300

    1 Sprint - ProdutoRepository

--- trabalhofinal/src/main/java/br/edu/ifg/poo2/trabalhofinal/repository/ProdutoRepository.java
+++ trabalhofinal/src/main/java/br/edu/ifg/poo2/trabalhofinal/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifg.poo2.trabalhofinal.entidades.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
+	
 }
